b4d73255b6835129e6c8aed19061810ce30b68ba
NoahNtabo23 <135032672+NoahNtabo23@users.noreply.github.com>
2023-06-09 17:49:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,65 @@
 var a, b
-beforeEach(function() {
-    a = Math.floor(Math.random() * 1000)
-    b = Math.floor(Math.random() * 1000)
-  })
-  parseInt("add",1)
+function add(a,b){
+  console.log(a+b)
+}
+add(3,4)
+
+function subtract(a,b){
+  console.log(b-a)
+}
+subtract(3,4)
+
+function multiply(a,b){
+  console.log(a*b)
+}
+multiply(3,4)
+
+function divide(a,b){
+  console.log(a/b)
+}
+divide(8,2)
+
+function add(a,b){
+  console.log(a+b)
+}
+add(3,4)
+
+function add(a,b){
+  return a+b
+}
+let total=add(1,3)
+
+function subtract(a,b){
+  return a-b
+}
+let answer=subtract(5,3)
+
+function multiply(a,b){
+  return a*b
+}
+let result=multiply(5,3)
+
+function divide(a,b){
+  return a/b
+}
+let returns=divide(8,4)
+
+function increment(n){
+  return ++n
+}
+let value=increment(5)
+
+function decrement(n){
+  return --n
+}
+let newvalue=decrement(6)
+
+function makeInt(n){
+  return parseInt(n,10)
+}
+let newreturn=makeInt("n")
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+let newanswer=preserveDecimal("20.25")
